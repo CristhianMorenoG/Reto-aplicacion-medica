892dd2184587439295243c0ac8240844c49d81cb
CristhianMorenoG <morenocristhian@outlook.com>
2023-02-11 14:30:55 -0500

    Se realiza creacion del Proyecto usando Spring Initializr y se generan las primeras clases abstractas

---
+++ src/main/java/Cristhian_Moreno_SofkaU/Proyecto_sistema_historias_medicas/clasesAbstractas/registroMedico.java
@@ -0,0 +1,4 @@
+package Cristhian_Moreno_SofkaU.Proyecto_sistema_historias_medicas.clasesAbstractas;
+
+public abstract class registroMedico {
+}
---
+++ src/test/java/Cristhian_Moreno_SofkaU/Proyecto_sistema_historias_medicas/ProyectoSistemaHistoriasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package Cristhian_Moreno_SofkaU.Proyecto_sistema_historias_medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSistemaHistoriasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
